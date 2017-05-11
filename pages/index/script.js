adb767749f5c4eda781b4158510f8a467f92f2ee
Elragos <alexandre.marechal.59@gmail.com>
2017-05-11 15:42:20 +0200

    Ajout appel ajax dans index

--- pages/index/script.js
+++ pages/index/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $("#sendAjax").click(function(){
         $.ajax({
-            url: "Simpage.do",
+            url: "index.do",
             method: "GET",
             data: $("#testAjax").serialize() 
         }).done(function(msg){
